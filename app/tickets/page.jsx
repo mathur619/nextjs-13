7c34b466fc926f9c36fc6278b2c6190aef2a7e49
mathur619 <prakhar@fyno.io>
2024-04-26 19:21:22 +0530

    11: Create ticket form - client side

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { Suspense } from 'react'
 import Loading from '../loading'
 import TicketList from './TicketList'
@@ -5,13 +6,16 @@ import TicketList from './TicketList'
 export default function Tickets() {
   return (
     <main>
-      <nav>
+      <nav className="flex justify-between ">
         <div>
           <h2>Tickets</h2>
           <p>
             <small>Currently open tickets.</small>
           </p>
         </div>
+        <Link href="/tickets/create">
+          <button className="btn-primary"> Create </button>
+        </Link>
       </nav>
       <Suspense fallback={<Loading />}>
         <TicketList />
